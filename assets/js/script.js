7de2042e6493aedfb2f971732afbc4d0478d4c10
daniel-pontes-fullstack <deslandev@gmail.com>
2023-10-14 10:09:55 -0300

    currency formated add

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,21 @@ const btnSwitchNow = document.querySelector(".convert-button")
 function convertValues(){
     
     const inputCurrencyValue = document.querySelector(".input-currency").value
-    
+    const currencyValueToCovert = document.querySelector(".currency-value-to-convert")
+    const currencyValueCoverted = document.querySelector(".currency-value")
     const dollarToday = 5.2
     const convertedValue = inputCurrencyValue / dollarToday
     
-    console.log(convertedValue)
+    currencyValueToCovert.innerHTML = new Intl.NumberFormat("pt-br", {
+        style: "currency",
+        currency: "BRL",
+    }).format(inputCurrencyValue)
+    
+    currencyValueCoverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+    }).format(convertedValue)
+
 }
 
 btnSwitchNow.addEventListener("click", convertValues)
