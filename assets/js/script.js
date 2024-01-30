496f26d3ea858631dbbfdec6bfd3799fd0bc5fa1
Daniel <deslandev@gmail.com>
2024-01-30 07:14:59 -0300

    Ateração de Informação

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const currencySelect = document.querySelector(".currency-select");
 const convertValues = async () =>  {
   const inputCurrencyValue = document.querySelector(".input-currency").value;
   const currencyValueToCovert = document.querySelector(".currency-value-to-convert");
-  //Async Await (Cosumo de API AwesomeAPI > awesomeapi.com.br teste em ter-30-jan-2024 )
+  //Async Await (Cosumo de API AwesomeAPI > awesomeapi.com.br teste em ter-30-jan-2024 Daniel )
   const data = await fetch ("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL").then(response => response.json())
  
   const currencyValueCoverted = document.querySelector(".currency-value");
